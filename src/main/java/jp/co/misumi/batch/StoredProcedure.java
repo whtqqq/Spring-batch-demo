f29f353288ef02df2d1886e829c38e8a214a4e4a
ys-Han <ys-han@rontech.co.jp>
2017-03-10 10:16:40 +0800

    Add uodate tasklet for update ondemand_shipment_mng.

---
+++ src/main/java/jp/co/misumi/batch/StoredProcedure.java
@@ -0,0 +1,42 @@
+package jp.co.misumi.batch;
+
+import javax.sql.DataSource;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class StoredProcedure implements Tasklet {
+	final Logger logger = LoggerFactory.getLogger(StoredProcedure.class);
+	
+	private DataSource dataSource;
+	private String sql;
+	
+	public DataSource getDataSource() {
+		return dataSource;
+	}
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+	public String getSql() {
+		return sql;
+	}
+	public void setSql(String sql) {
+		this.sql = sql;
+	}
+
+	@Override
+	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+		
+		JdbcTemplate jdbc = new JdbcTemplate(getDataSource());
+		// update 
+		jdbc.execute(getSql());
+		
+		return RepeatStatus.FINISHED;
+	}
+
+}
