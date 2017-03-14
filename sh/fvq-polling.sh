336828cfc8aaa729c2382e75073b8da9ed8351d8
EC2 Default User <ec2-user@ip-172-31-3-216.us-west-1.compute.internal>
2017-03-14 13:47:44 +0000

    Fix some bugs in scripts

--- sh/fvq-polling.sh
+++ sh/fvq-polling.sh
@@ -1,9 +1,28 @@
 #!/bin/sh
+########################################
+# stop function
+########################################
+function stop_polling() {
+  echo "received temination signal."
+  stop_flg=1
+}
+
+########################################
+# run the job 
+########################################
+function run_job() {
+  fvq_home=/home/ec2-user/FVQ
+  lib=$fvq_home/lib
+  log=$fvq_home/logs/fvq-magi-link-job.log
+  target=$fvq_home/target/fvq.jar
+  job_runner=org.springframework.batch.core.launch.support.CommandLineJobRunner
+
+  java -cp $target:$lib/* $job_runner classpath:/batch-context.xml FVQ_MagiLinkJob >> $log 2>&1
+}
 
 # trap termination signal
 ########################################
 trap "stop_polling" 2 15
-
 
 # polling
 ########################################
@@ -24,24 +43,3 @@ do
    fi
 done
 exit 0
-
-########################################
-# stop function
-########################################
-function stop_polling() {
-  echo "received teminate signal."
-  stop_flg=1
-}
-
-########################################
-# run the job 
-########################################
-function run_job() {
-  fvq_home=/home/ec2-user/FVQ
-  lib=$fvq_home/lib
-  logs=$fvq_home/logs
-  target=$fvq_home/target/fvq.jar
-  job_runner=org.springframework.batch.core.launch.support.CommandLineJobRunner
-
-  java -cp $target:$lib/* $job_runner classpath:/batch-context.xml FVQ_MagiLinkJob >> $logs/fvq-magi-link-job.log 2>&1
-}
--- sh/remove-fvq-service.sh
+++ sh/remove-fvq-service.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
+# check fvq service
+######################################## 
+cnt=`chkconfig --list  | grep fvq | wc -l`
+if [ $cnt -eq 0 ]; then
+  echo "fvq service is not registered." 
+  exit 0 
+fi
+
 # remove service
 ######################################## 
 chkconfig --del fvq
 cnt=`chkconfig --list  | grep fvq | wc -l`
 if [ $cnt -gt 0 ]; then
-  echo "faild to remove fvq service." 
+  echo "failed to remove fvq service." 
   exit -1
 fi
+
+# remove fvq file from /etc/init.d
+######################################## 
+rm -f /etc/init.d/fvq
 
 echo "Succeeded in removing faq service!" 
 exit 0
